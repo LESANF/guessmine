ef7625e09644f0f78b2a426d5ba7ef188f9268fa
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-03-26 22:08:24 +0900

    Just BS

--- src/socketController.js
+++ src/socketController.js
@@ -3,7 +3,8 @@ import events from "./events";
 const sockets = [];
 
 const socketController = io => socket => {
-  const notifyNewUser = nickname => socket.emit(events.newUser, { nickname });
+  const notifyNewUser = nickname =>
+    socket.broadcast.emit(events.newUser, { nickname });
   socket.on(events.login, ({ nickname }) => {
     socket.name = nickname;
     sockets.push(socket.id);
